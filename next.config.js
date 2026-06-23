f8828409112e0de23727da7b1bf46a0017e1ba5f
fazetdev <fazetdev@gmail.com>
2026-06-23 14:42:35 +0100

    fix vercel ws build issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'bufferutil', 'utf-8-validate'];
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
